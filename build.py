3d16129e973eb1cb728bd5b973b72a028caa6c91
xenxia <xen.devops@gmail.com>
2023-06-03 09:42:38 +0200

    Change build script

--- build.py
+++ build.py
@@ -1,12 +1,41 @@
 import argparse
-import os, pathlib, shutil
+import os, pathlib, shutil, json, platform
 import PyInstaller.__main__
 import compileall
 
-INDEX = "index"
-APP_NAME = "SortedTree"
+PLATFORM_SYS = platform.system()
 
-parser = argparse.ArgumentParser(description="app to build Tree",
+CONFIG_FILE = "build.json"
+
+CONFIG_TEMPLATE = '''
+{   
+    // Name App
+    "name_app": "",
+
+    // Entry script
+    "index": "",
+
+    // Icon App
+    "icon": "",
+
+    // Build on one file
+    "onefile": true,
+
+    // Clean build file
+    "clean": false,
+
+    // Contains pre-build scripts for the app
+    //Move to: parentDir/comp
+    "compile_dir": [],
+
+    // Add File or Dir | img;img (Dir img to Dir img)
+    "add_data": [],
+
+    // Import not in entry script
+    "hidden_import": []
+}
+'''
+parser = argparse.ArgumentParser(description="app to build",
                                  allow_abbrev=False,
                                  prog='Build',
                                  usage='%(prog)s'
@@ -19,6 +48,9 @@ subparsers = parser.add_subparsers(title='Commande',
 parser_dev = subparsers.add_parser('dev', help='Build app development',
                                     formatter_class=argparse.RawTextHelpFormatter)
 
+parser_dev = subparsers.add_parser('config', help='Générate template config',
+                                    formatter_class=argparse.RawTextHelpFormatter)
+
 # parser_prod = subparsers.add_parser('prod', help='Build app production',
 #                                     formatter_class=argparse.RawTextHelpFormatter)
 
@@ -27,84 +59,73 @@ group.add_argument('-v', '--version', action='version', version="V1")
 
 args = parser.parse_args()
 
-compileall.compile_dir(dir="func", legacy=True, force=True)
-compileall.compile_dir(dir="page", legacy=True, force=True)
+#load file config
 
-path_comp = ["./func", "./page"]
+with open(CONFIG_FILE, 'r', encoding='utf8') as file:
+    config = json.load(file)
 
-for p in path_comp:
-    p = f"{p}/comp"
-    if os.path.exists(p):
-        shutil.rmtree(p)
+if config["compile_dir"] != []:
+    for path in config["compile_dir"]:
 
-for p in path_comp:
-    p = f"{p}/comp"
-    os.mkdir(p)
+        compileall.compile_dir(dir=path, legacy=True, force=True)
 
-# move file.pyc to ./comp
-for p in path_comp:
-    for file in pathlib.Path(p).glob("*.pyc"):
-        os.rename(str(file), f"{p}/comp/{file.name}")
+        path_comp = f"./{path}/comp"
+        if os.path.exists(path_comp):
+            shutil.rmtree(path_comp)
+        os.mkdir(path_comp)
+        for file in pathlib.Path(f"./{path}").glob("*.pyc"):
+            os.rename(str(file), f"{path_comp}/{file.name}")
 
 if args.Command_Name == "dev":
 
+    arg_dev = [
+        config["index"],
+        f"--name={config['name_app']}_dev",
+    ]
+
+    if config["onefile"]: arg_dev.append("--onefile")
+    if config["clean"]: arg_dev.append("--clean")
+    if config["icon"] != "": arg_dev.append(f"--icon={config['icon']}")
+
+    if config["add_data"] != []:
+        for i in config["add_data"]:
+            arg_dev.append(f"--add-data={i}")
+
+    if config["hidden_import"] != []:
+        for i in config["hidden_import"]:
+            arg_dev.append(f"--hidden-import={i}")
+
     print("\n=========================================== BUILD DEV ===========================================\n")
-    PyInstaller.__main__.run([
-        f'{INDEX.lower()}.py',
-        f'--name={APP_NAME}_dev',
-        '--onefile',
-        # '--clean',
-        '--add-data=func/comp;func',
-        '--add-data=page/comp;page',
-        '--add-data=img;img',
-        '--add-data=lang;lang',
-        '--add-data=themes;themes',
-        '--hidden-import=locale',
-        '--hidden-import=ruamel.yaml',
-        '--hidden-import=typing',
-        '--hidden-import=ctypes',
-        '--hidden-import=sys',
-        '--hidden-import=tkinter.ttk',
-        '--hidden-import=tkinter.scrolledtext',
-        '--hidden-import=tkinter.filedialog',
-        '--hidden-import=tkinter',
-        '--hidden-import=requests',
-        '--hidden-import=webbrowser',
-        '--hidden-import=Tk-up',
-        '--icon=img/tree.ico'
-    ])
+    PyInstaller.__main__.run(arg_dev)
     print("\n========================================= END BUILD DEV ==========================================\n")
 
 else:
+
+    arg = [
+        config["index"],
+        f"--name={config['name_app']}",
+    ]
+
+    if PLATFORM_SYS == "Windows":
+        arg.append("--windowed")
+
+    if config["onefile"]: arg.append("--onefile")
+    if config["clean"]: arg.append("--clean")
+    if config["icon"] != "": arg.append(f"--icon={config['icon']}")
+
+    if config["add_data"] != []:
+        for i in config["add_data"]:
+            arg.append(f"--add-data={i}")
+
+    if config["hidden_import"] != []:
+        for i in config["hidden_import"]:
+            arg.append(f"--hidden-import={i}")
+
     print("=========================================== BUILD PROD ===========================================\n")
-    PyInstaller.__main__.run([
-        f'{INDEX.lower()}.py',
-        f'--name={APP_NAME}',
-        '--onefile',
-        # '--clean',
-        '--windowed',
-        '--add-data=func/comp;func',
-        '--add-data=page/comp;page',
-        '--add-data=img;img',
-        '--add-data=lang;lang',
-        '--add-data=themes;themes',
-        '--hidden-import=locale',
-        '--hidden-import=ruamel.yaml',
-        '--hidden-import=typing',
-        '--hidden-import=ctypes',
-        '--hidden-import=sys',
-        '--hidden-import=tkinter.ttk',
-        '--hidden-import=tkinter.scrolledtext',
-        '--hidden-import=tkinter.filedialog',
-        '--hidden-import=tkinter',
-        '--hidden-import=requests',
-        '--hidden-import=webbrowser',
-        '--hidden-import=Tk-up',
-        '--icon=img/tree.ico'
-    ])
+    PyInstaller.__main__.run(arg)
     print("\n========================================= END BUILD PROD ==========================================\n")
 
-for p in path_comp:
-    p = f"{p}/comp"
-    if os.path.exists(p):
-        shutil.rmtree(p)
+if args.Command_Name == "config":
+    
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(CONFIG_TEMPLATE, f)
