ab77cac77d9d6744e138cfb4a495704a64474dc5
Xenxia <xen.devops@gmail.com>
2023-08-03 12:26:23 +0200

    Add Info system and copy button

--- page/about.py
+++ page/about.py
@@ -1,6 +1,6 @@
 from typing import Any
-import webbrowser
-from tk_up.widgets.frame import Frame_up
+import webbrowser, pyperclip
+from tk_up.widgets.frame import Frame_up, LabelFrame_up
 from tk_up.widgets.button import Button_up
 from tk_up.widgets.label import Label_up
 from tk_up.managerWidgets import ManagerWidgets_up
@@ -8,6 +8,7 @@ from tkinter import E, W, S, N
 import datetime
 from Pylogger import Logger
 from Pylang import Lang
+from func.conf import Config_, PLATFORM_SYS
 
 class about(Frame_up):
 
@@ -20,6 +21,7 @@ class about(Frame_up):
         self.wManager = wManager
 
         self.langs: Lang = self.ctx["lib"][0]
+        self.conf: Config_ = self.ctx["lib"][1]
         self.log: Logger = self.ctx["lib"][2]
 
         # Use 'self' in your widget
@@ -29,18 +31,28 @@ class about(Frame_up):
 
         # ==
 
-        self.about_text = Label_up(self, text=f"Developper : Xenxia\nOrganisation : Cheese-grinder\n\n© {datetime.date.today().year} Cheese-grinder")
-        self.about_text.placePosSize(350, 100, 250, 140, anchor="center").show()
+        self.aboutLabel = Label_up(self, text=f"Developper : Xenxia\nOrganisation : Cheese-grinder\n\n© {datetime.date.today().year} Cheese-grinder")
+        self.aboutLabel.placePosSize(350, 100, 250, 140, anchor="center").show()
 
-        self.button_link = Button_up(self, text=f"Project Link : https://github.com/Xenxia/{self.ctx['app_name']}", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}'), style="link.TButton")
-        self.button_link.placePosSize(350, 200, 350, 24, anchor="center").show()
+        self.linkBtn = Button_up(self, text=f"Project Link : https://github.com/Xenxia/{self.ctx['app_name']}", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}'), style="link.TButton")
+        self.linkBtn.placePosSize(350, 200, 350, 24, anchor="center").show()
 
-        self.button_link = Button_up(self, text=f"Create New Issues", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}/issues/new/choose'))
-        self.button_link.placePosSize(350, 230, 150, 24, anchor="center").show()
+        self.linkBtn2 = Button_up(self, text=f"Create New Issues", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}/issues/new/choose'))
+        self.linkBtn2.placePosSize(350, 230, 150, 24, anchor="center").show()
+
+
+        self.sysFrame = LabelFrame_up(self, text="Info System")
+        self.sysFrame.placePosSize(350, 360, 310, 200, anchor="center").show()
+        self.sysFrame.columnconfigure(0, weight=1)
+
+        self.sysLabel = Label_up(self.sysFrame, text=" Error")
+        self.sysLabel.gridPosSize(row=0, column=0, sticky=(E, W, S, N)).show()
+
+        self.button_return = Button_up(self, text="Copy sys info", command=lambda: pyperclip.copy(self.t))
+        self.button_return.placePosSize(350, 490, 130, 24, anchor="center").show()
 
         self.button_return = Button_up(self, text=self.langs.t('UI.ABOUT.button_return'), command=lambda: wManager.showWidget("option"))
         self.button_return.placePosSize(350, 550, 120, 24, anchor="center").show()
-
 
     # this function is call if you hide widget
     def disable(self):
@@ -48,4 +60,18 @@ class about(Frame_up):
 
      # this function is call if you show widget
     def enable(self):
-        pass
+        systemInfo = [
+            f"System Lang : {self.langs.getLocaleSys()}",
+            f"System OS : {PLATFORM_SYS}",
+            f"App version : {self.ctx['app_version']}",
+            f"App last version : {self.ctx['app_last_version']}",
+        ]
+
+        self.t = ""
+        for i in systemInfo:
+            self.t = self.t + f" {i}\n"
+
+        self.sysLabel.configure(text=self.t)
+        self.sysLabel.update()
+
+        
