34cb6803ce9f1dd6a9159b33353097496c4e20ad
Xenxia <xen.devops@gmail.com>
2023-08-07 13:45:47 +0200

    Add button to about page

--- page/about.py
+++ page/about.py
@@ -1,13 +1,19 @@
 from typing import Any
-import webbrowser, pyperclip
+import webbrowser, pyperclip, datetime
+
 from tk_up.widgets.frame import Frame_up, LabelFrame_up
 from tk_up.widgets.button import Button_up
 from tk_up.widgets.label import Label_up
+
 from tk_up.managerWidgets import ManagerWidgets_up
+
+from tk_up.object.image import Wimage
+
 from tkinter import E, W, S, N
-import datetime
+
 from Pylogger import Logger
 from Pylang import Lang
+
 from func.conf import Config_, PLATFORM_SYS
 
 class about(Frame_up):
@@ -15,6 +21,9 @@ class about(Frame_up):
     # DONT REMOVE THIS
     ctx: dict[str, Any]
     wManager: ManagerWidgets_up
+
+    ###
+    textInfoSys: str
 
     def __init__(self, context: dict[str, Any], wManager: ManagerWidgets_up, master, kw={"width":0, "height":0}):
         self.ctx = context.copy()
@@ -34,22 +43,27 @@ class about(Frame_up):
         self.aboutLabel = Label_up(self, text=f"Developper : Xenxia\nOrganisation : Cheese-grinder\n\n© {datetime.date.today().year} Cheese-grinder")
         self.aboutLabel.placePosSize(350, 100, 250, 140, anchor="center").show()
 
-        self.linkBtn = Button_up(self, text=f"Project Link : https://github.com/Xenxia/{self.ctx['app_name']}", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}'), style="link.TButton")
-        self.linkBtn.placePosSize(350, 200, 350, 24, anchor="center").show()
+        self.linkBtn = Button_up(self, text="github", image=Wimage(self.ctx["exe_path"]+"/img/github.png", (36, 36)), command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}'), style="nobg.TButton")
+        self.linkBtn.placePosSize(330, 160, 38, 38, anchor="center").show()
+
+        self.linkBtn = Button_up(self, text="wiki", image=Wimage(self.ctx["exe_path"]+"/img/info.png", (36, 36)), command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}/wiki'), style="nobg.TButton")
+        self.linkBtn.placePosSize(370, 160, 38, 38, anchor="center").show()
+
+
 
         self.linkBtn2 = Button_up(self, text=f"Create New Issues", command=lambda: webbrowser.open(f'https://github.com/Xenxia/{self.ctx["app_name"]}/issues/new/choose'))
-        self.linkBtn2.placePosSize(350, 230, 150, 24, anchor="center").show()
+        self.linkBtn2.placePosSize(350, 210, 150, 24, anchor="center").show()
 
 
         self.sysFrame = LabelFrame_up(self, text="Info System")
-        self.sysFrame.placePosSize(350, 360, 310, 200, anchor="center").show()
+        self.sysFrame.placePosSize(350, 330, 310, 200, anchor="center").show()
         self.sysFrame.columnconfigure(0, weight=1)
 
         self.sysLabel = Label_up(self.sysFrame, text=" Error")
-        self.sysLabel.gridPosSize(row=0, column=0, sticky=(E, W, S, N)).show()
+        self.sysLabel.packPosSize(fill="both").show()
 
-        self.button_return = Button_up(self, text="Copy sys info", command=lambda: pyperclip.copy(self.t))
-        self.button_return.placePosSize(350, 490, 130, 24, anchor="center").show()
+        self.button_return = Button_up(self.sysFrame, text="Copy sys info", image=Wimage(self.ctx["exe_path"]+"/img/copy.png", (28, 28)), command=lambda: pyperclip.copy(self.textInfoSys), style="nobg.TButton")
+        self.button_return.placePosSize(290, 25, 30, 30, anchor="center").show()
 
         self.button_return = Button_up(self, text=self.langs.t('UI.ABOUT.button_return'), command=lambda: wManager.showWidget("option"))
         self.button_return.placePosSize(350, 550, 120, 24, anchor="center").show()
@@ -67,11 +81,11 @@ class about(Frame_up):
             f"App last version : {self.ctx['app_last_version']}",
         ]
 
-        self.t = ""
+        self.textInfoSys = ""
         for i in systemInfo:
-            self.t = self.t + f" {i}\n"
+            self.textInfoSys = self.textInfoSys + f" {i}\n"
 
-        self.sysLabel.configure(text=self.t)
+        self.sysLabel.configure(text=self.textInfoSys)
         self.sysLabel.update()
 
         
